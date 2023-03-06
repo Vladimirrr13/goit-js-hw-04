ee45a5eea7c25435b5aed96a5b1e23f7af7429d9
Vladimirrr13 <99765660+Vladimirrr13@users.noreply.github.com>
2023-03-06 23:27:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
 /* --------------------------------- TASK 1 --------------------------------- */
 
-/*  Запиши условие в инструкции if так, чтобы функция работала правильно.
-Объявлена функция checkAge(age).
-В выражении проверки возраста использован оператор >=
-Вызов checkAge(20) возвращает "You are an adult"
-Вызов checkAge(8) возвращает "You are a minor"
-Вызов checkAge(14) возвращает "You are a minor"
-Вызов checkAge(38) возвращает "You are an adult"
-В теле функции есть только одна инструкция if
-В теле функции нет инструкции else или else if  */
+/*  Дополни код так, чтобы в переменной result был результат выполнения функции makePizza, а в переменной pointer была ссылка на функцию makePizza.
+
+Объявлена функция makePizza
+Объявлена переменная result
+Значение переменной result это строка "Your pizza is being prepared, please wait."
+Значение переменной result получено с помощью вызова функции
+Объявлена переменная pointer
+Значение переменной pointer это ссылка на функцию makePizza  */
 
 /* 
-function checkAge(age) {
-  if (age >= 18) { // Change this line
-    return "You are an adult";
-  }
-  return "You are a minor";
-}
-console.log(checkAge(20));
-console.log(checkAge(8));
-console.log(checkAge(14));
-console.log(checkAge(38));
+
+
+
 */
