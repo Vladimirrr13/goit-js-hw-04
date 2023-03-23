e5d68d8a74ec553dca844e2d404ddc91f1ca736f
Vladimirrr13 <99765660+Vladimirrr13@users.noreply.github.com>
2023-03-23 23:48:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -50,3 +50,115 @@ return callback(pizzaName);
 
 console.log(makeMessage("Royal Grand", makePizza));
 console.log(makeMessage("Ultracheese", deliverPizza));   */
+
+
+
+/* --------------------------------- TASK 3 --------------------------------- */
+
+/*
+Дополни второй вызов функции makePizza(pizzaName, callback), передав вторым аргументом инлайн колбэк-функцию eatPizza(pizzaName), которая логирует строку "Eating pizza <имя пиццы>".
+
+Объявлена функция makePizza
+Функция makePizza принимает два параметра
+Вторым аргументом при вызове makePizza("Ultracheese") передана функция eatPizza с единственным параметром pizzaName
+*/
+
+/*
+function makePizza(pizzaName, callback) {
+  console.log(`Pizza ${pizzaName} is being prepared, please wait...`);
+  callback(pizzaName);
+}
+
+makePizza("Royal Grand", function deliverPizza(pizzaName) {
+  console.log(`Delivering pizza ${pizzaName}.`);
+});
+
+// Change code below this line
+
+makePizza("Ultracheese", function eatPizza(pizzaName) {
+  console.log(`Eating pizza ${pizzaName}.`);
+});
+*/
+
+
+
+
+/* --------------------------------- TASK 4 --------------------------------- */
+
+/*
+Необходимо написать логику обработки заказа пиццы. Выполни рефакторинг метода order так, чтобы он принимал вторым и третим параметрами два колбэка onSuccess и onError.
+
+Если в свойстве pizzas нет пиццы с названием из параметра pizzaName, метод order должен возвращать результат вызова колбэка onError, передавая ему аргументом строку "There is no pizza with a name <имя пиццы> in the assortment."
+Если в свойстве pizzas есть пицца с названием из параметра pizzaName, метод order должен возвращать результат вызова колбэка onSuccess, передавая ему аргументом имя заказанной пиццы.
+После объявления объекта pizzaPalace мы добавили колбэки и вызовы методов. Пожалуйста ничего там не меняй.
+
+Метод order объявляет три параметра
+Вызов pizzaPalace.order("Smoked", makePizza, onOrderError) возвращает "Your order is accepted. Cooking pizza Smoked."
+Вызов pizzaPalace.order("Four meats", makePizza, onOrderError) возвращает "Your order is accepted. Cooking pizza Four meats."
+Вызов pizzaPalace.order("Big Mike", makePizza, onOrderError) возвращает "Error! There is no pizza with a name Big Mike in the assortment."
+Вызов pizzaPalace.order("Vienna", makePizza, onOrderError) возвращает "Error! There is no pizza with a name Vienna in the assortment."
+*/
+
+
+/* const pizzaPalace = {
+  pizzas: ['Ultracheese', 'Smoked', 'Four meats'],
+  order(pizzaName, onSuccess, onError) {
+    if (this.pizzas.includes(pizzaName)) {
+      return onSuccess(pizzaName);
+    }
+    return onError(`There is no pizza with a name ${pizzaName} in the assortment.`);
+  },
+};
+
+// Callback for onSuccess
+function makePizza(pizzaName) {
+  return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+}
+
+// Callback for onError
+function onOrderError(error) {
+  return `Error! ${error}`;
+}
+
+// Method calls with callbacks
+pizzaPalace.order('Smoked', makePizza, onOrderError);
+pizzaPalace.order('Four meats', makePizza, onOrderError);
+pizzaPalace.order('Big Mike', makePizza, onOrderError);
+pizzaPalace.order('Vienna', makePizza, onOrderError);
+  */
+
+
+
+
+/* --------------------------------- TASK 5 --------------------------------- */
+
+/* Функция calculateTotalPrice(orderedItems) принимает один параметр orderedItems - массив чисел, и рассчитывает общую сумму его элементов, которая сохраняется в переменной totalPrice и возвращается как результат работы функции.
+
+Выполни рефакторинг функции так, чтобы вместо цикла for она использовала метод forEach.
+
+Объявлена функция calculateTotalPrice(orderedItems)
+Для перебора массива orderedItems использован метод forEach
+Вызов функции calculateTotalPrice([12, 85, 37, 4]) возвращает 138
+Вызов функции calculateTotalPrice([164, 48, 291]) возвращает 503
+Вызов функции calculateTotalPrice([412, 371, 94, 63, 176]) возвращает 1116
+Вызов функции со случайными, но валидными аргументами, возвращает правильное значение
+
+*/
+
+
+/* function calculateTotalPrice(orderedItems) {
+let totalPrice = 0;
+orderedItems.forEach(function(item) {
+totalPrice += item;
+});
+return totalPrice;
+}
+
+console.log(calculateTotalPrice([12, 85, 37, 4]));
+console.log(calculateTotalPrice([164, 48, 291])); 
+console.log(calculateTotalPrice([412, 371, 94, 63, 176]));   */
+
+
+
+
+
